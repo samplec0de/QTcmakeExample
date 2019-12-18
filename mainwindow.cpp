6d74f1f607d32dcb88be0d8dedaf793020196d97
Andrew Moscalev <admin@asimple.ru>
2019-12-18 23:32:40 +0300

    Initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Андрей Москалёв on 2019-10-14.
+//
+
+#include "mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
+{
+    this->setStyleSheet("");
+}
+
+MainWindow::~MainWindow() = default;
